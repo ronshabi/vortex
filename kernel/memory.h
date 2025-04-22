f0c8ff228f7a1ef7e531507681335ba2375e92be
Ron Shabi <ron@ronsh.net>
2025-04-22 20:39:34 +0300

    kernel/memory: inline functions, shorten names

--- kernel/memory.h
+++ kernel/memory.h
@@ -1,9 +1,44 @@
 #pragma once
 
-#include <stdint.h>
 #include <stddef.h>
+#include <stdint.h>
 
-void memory_write_u32(uint64_t address, uint32_t value);
-void memory_write_u8(uint64_t address, uint8_t value);
-uint32_t memory_read_u32(uint64_t address);
-uint32_t memory_read_u32_bswap(uint64_t address);
+static inline void mem_write_u8(uint64_t address, uint8_t value)
+{
+    *((volatile uint8_t *)address) = value;
+}
+
+static inline void mem_write_u16(uint64_t address, uint16_t value)
+{
+    *((volatile uint16_t *)address) = value;
+}
+
+static inline void mem_write_u32(uint64_t address, uint32_t value)
+{
+    *((volatile uint32_t *)address) = value;
+}
+
+static inline void mem_write_u64(uint64_t address, uint64_t value)
+{
+    *((volatile uint64_t *)address) = value;
+}
+
+static inline uint8_t mem_read_u8(uint64_t address)
+{
+    return *((volatile uint8_t *)address);
+}
+
+static inline uint16_t mem_read_u16(uint64_t address)
+{
+    return *((volatile uint16_t *)address);
+}
+
+static inline uint32_t mem_read_u32(uint64_t address)
+{
+    return *((volatile uint32_t *)address);
+}
+
+static inline uint64_t mem_read_u64(uint64_t address)
+{
+    return *((volatile uint64_t *)address);
+}
